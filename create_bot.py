19a13a747ae6253f9bedc4027ae57b2e8db7d832
Max <maks6863@gmail.com>
2023-05-29 23:31:28 +0500

    Update create_bot.py
    Del token

--- create_bot.py
+++ create_bot.py
@@ -6,7 +6,7 @@ from DataBase.db import Database
 
 
 
-TOKEN = "5743429635:AAFcQlpWeAHdGRByCVuRJWCP7xNYQzxYsJ8"
+TOKEN = "TOKEN"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 db = Database('DataBase/database.db')
